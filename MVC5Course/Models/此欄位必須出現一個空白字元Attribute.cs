87f2eb05a4393281122afff50094b4d8a7993c9f
Lucien <lucien@miniasp.com>
2016-03-06 17:01:52 +0800

    練習自訂 Models 的驗證屬性 ( 繼承 DataTypeAttribute )

---
+++ MVC5Course/Models/此欄位必須出現一個空白字元Attribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC5Course.Models
+{
+    internal class 此欄位必須至少出現一個空白字元Attribute : DataTypeAttribute
+    {
+        public 此欄位必須至少出現一個空白字元Attribute() : base(DataType.Text)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            return ((string)value).Contains(" ");
+        }
+    }
+}
