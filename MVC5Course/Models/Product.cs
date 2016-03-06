90cc148e891e126b5262e4319f9251c0ff78161c
Lucien <lucien@miniasp.com>
2016-03-06 15:16:16 +0800

    練習 EF 設計工具內「從資料庫更新模型」的各種 SSDL, CSDL, MSL 更新方法 新建View的語法：CREATE VIEW [dbo].[vwClientOrder] AS   SELECT client.clientid,          client.firstname,          client.lastname,          [order].orderdate,          [order].ordertotal,          [order].orderstatus   FROM   client          INNER JOIN [order]                  ON client.clientid = [order].clientid

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,7 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,18 +19,13 @@ namespace MVC5Course.Models
         {
             this.OrderLine = new HashSet<OrderLine>();
         }
-
+    
         public int ProductId { get; set; }
-        [Required]
         public string ProductName { get; set; }
-        //[Display(Name = "商品價錢"), Required, Range(2, 99, ErrorMessage = "{0}輸入錯誤,應介於{1}與{2}之間")]
-        [DisplayFormat(DataFormatString = "{0:C}")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
         public Nullable<decimal> Stock { get; set; }
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
